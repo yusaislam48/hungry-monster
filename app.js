eb811cd655ac3235cf6971d7c53cbdc9ccd05a82
Yusa Islam <yusaislam48@gmail.com>
2021-02-08 01:07:52 +0600

    added navbar and logo

--- app.js
+++ app.js
@@ -4,26 +4,32 @@ const searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', function () {
     document.getElementById("mealsList").innerHTML = "";
     document.getElementById('warningSec').style.display = 'none';
+    document.getElementById('ingradientsSec').style.display = 'none';
+    document.getElementById('mealSec').style.display = 'block';
     getMeal(inputMeal);
 })
 
-// display all meal by search 
-const getMeal = inputMeal => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeal.value}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMeals(data.meals))
-        .catch(error => {
-            alert('Wrong Meal Name!')
-            document.getElementById('warningSec').style.display = 'block';
 
-        })
+
+// display all meal by search 
+const getMeal = async inputMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeal.value}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMeals(data.meals);
+    } catch (error) {
+        document.getElementById('warningSec').innerHTML = `
+            <h1>Wrong Meal Name!</h1>
+        `;
+        document.getElementById('warningSec').style.display = 'block';
+    }    
 }
 const displayMeals = meals =>{
     meals.forEach(meals => {
         const mealsListDiv = document.getElementById('mealsList');
             const meal = `
-                <div onClick = "displayMealDetails('${meals.idMeal}')">
+                <div onClick = "getMealDetails('${meals.idMeal}')">
                     <img class="img-fluid" src="${meals.strMealThumb}" alt=""><br>
                     <h4 class='text-center'>${meals.strMeal}</h4>
                 </div>
@@ -35,35 +41,46 @@ const displayMeals = meals =>{
     });              
 }
 
+
+
 // display meal ingradients by click
-const displayMealDetails = mealId => {
+const getMealDetails = async mealId => {
+    
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    try { 
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMealDetails(data);
+    } catch (error) {
+        document.getElementById('warningSec').innerHTML = `
+            <h1>Please try again later!</h1>
+        `;
+        document.getElementById('warningSec').style.display = 'block';
+    }
+}
+const displayMealDetails = data =>{
     document.getElementById("mealsDetails").innerHTML = "";
     const ingradientsSec = document.getElementById('ingradientsSec');
     ingradientsSec.style.display = 'block';
     const mealSec = document.getElementById('mealSec');
     mealSec.style.display = 'none';
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.meals[0]);
-            const mealsDetails = document.getElementById('mealsDetails');
-            mealsDetails.innerHTML = `
-                <button onClick = "backButton()" id="backBtn" type="button" class="btn btn-warning mb-1">back></button>
-                <img class="img-fluid rounded mx-auto d-block" style="width:400px;" src="${data.meals[0].strMealThumb}" alt="images">
-                <h2 class="fw-bold md-1">${data.meals[0].strMeal}</h2><br>
-                <h4>Ingredients</h4>
-                <ul>
-                    <li>${data.meals[0].strIngredient1}</li>
-                    <li>${data.meals[0].strIngredient2}</li>
-                    <li>${data.meals[0].strIngredient3}</li>
-                    <li>${data.meals[0].strIngredient4}</li>
-                    <li>${data.meals[0].strIngredient5}</li>
-                    <li>${data.meals[0].strIngredient6}</li>
-                    <li>${data.meals[0].strIngredient7}</li>
-                </ul>   
-            `
-        })
+    // console.log(data.meals[0]);
+    const mealsDetails = document.getElementById('mealsDetails');
+    mealsDetails.innerHTML = `
+        <button onClick = "backButton()" id="backBtn" type="button" class="btn btn-warning mb-1">back></button>
+        <img class="img-fluid rounded mx-auto d-block" style="width:400px;" src="${data.meals[0].strMealThumb}" alt="images">
+        <h2 class="fw-bold md-1">${data.meals[0].strMeal}</h2><br>
+        <h4>Ingredients</h4>
+        <ul>
+            <li>${data.meals[0].strIngredient1}</li>
+            <li>${data.meals[0].strIngredient2}</li>
+            <li>${data.meals[0].strIngredient3}</li>
+            <li>${data.meals[0].strIngredient4}</li>
+            <li>${data.meals[0].strIngredient5}</li>
+            <li>${data.meals[0].strIngredient6}</li>
+            <li>${data.meals[0].strIngredient7}</li>
+            </ul>   
+     `
 }
 
 // back button 
